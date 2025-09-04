04995525c56a1dfd0f50f8673fb9c7cad1a72d24
UpendraChaturvedi <upendrachaturvedi099@gmail.com>
2025-09-04 11:59:23 +0530

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ 2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,12 @@
+// Last updated: 9/4/2025, 11:59:22 AM
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
