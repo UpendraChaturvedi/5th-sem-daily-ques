e76d331ec98c6fefb7675cec58cc89a3bde47bcd
UpendraChaturvedi <upendrachaturvedi099@gmail.com>
2025-09-04 11:59:41 +0530

    Create 2556-ConvertTheTemperature

---
+++ 2556-ConvertTheTemperature/2556-ConvertTheTemperature.java
@@ -0,0 +1,9 @@
+// Last updated: 9/4/2025, 11:59:40 AM
+class Solution {
+    public double[] convertTemperature(double celsius) {
+        double[] arr=new double[2];
+        arr[1]=1.80*celsius+32.00;
+        arr[0]=celsius+273.15;
+        return arr;
+    }
+}
