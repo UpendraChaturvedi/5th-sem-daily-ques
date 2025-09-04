1f0ea36e5696ecbf42b552562637ee2f9f368d2c
UpendraChaturvedi <upendrachaturvedi099@gmail.com>
2025-09-04 12:03:48 +0530

    Create 669-TrimABinarySearchTree

---
+++ 669-TrimABinarySearchTree/669-TrimABinarySearchTree.java
@@ -0,0 +1,32 @@
+// Last updated: 9/4/2025, 12:03:48 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode trimBST(TreeNode root, int l, int h) {
+        if(root==null){
+            return null;
+        }
+        if(root.val<l){
+            return trimBST(root.right,l,h);
+        }if(root.val>h){
+            return trimBST(root.left,l,h);
+        }
+        root.left=trimBST(root.left,l,h);
+        root.right=trimBST(root.right,l,h);
+        return root;
+        
+    }
+}
