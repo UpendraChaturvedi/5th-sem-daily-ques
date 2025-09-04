53e811e2355c4ee9ff4c107951e1829fd6a7f8f1
UpendraChaturvedi <upendrachaturvedi099@gmail.com>
2025-09-04 12:05:55 +0530

    Create 275-HIndexIi

---
+++ 275-HIndexIi/275-HIndexIi.java
@@ -0,0 +1,26 @@
+// Last updated: 9/4/2025, 12:05:54 PM
+class Solution {
+    public int hIndex(int[] citations) {
+        
+		  
+		
+		  
+		  int n = citations.length;
+		  int s=0;
+		  int e = n-1;
+		  int a=0;
+		  while(s<=e) {	
+			  int m = (s+e)/2;
+			  if(citations[m]>=n-m) {
+				  e=m-1;
+				  a=n-m;
+			  }else {
+				  s=m+1;
+			  }
+		  }
+		  
+		  return a;
+	   
+	   
+    }
+}
