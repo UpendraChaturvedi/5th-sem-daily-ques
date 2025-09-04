8f993a013a2f88f8b8aa6f6a69bcf8759a1a908f
UpendraChaturvedi <upendrachaturvedi099@gmail.com>
2025-09-04 11:59:13 +0530

    Create 3194-FindWordsContainingCharacter

---
+++ 3194-FindWordsContainingCharacter/3194-FindWordsContainingCharacter.java
@@ -0,0 +1,12 @@
+// Last updated: 9/4/2025, 11:59:13 AM
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < words.length; ++i) {
+            if (words[i].indexOf(x) != -1) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
