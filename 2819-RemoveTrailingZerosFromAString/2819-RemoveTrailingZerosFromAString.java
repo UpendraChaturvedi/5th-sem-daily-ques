c30f8472836920d7c856405efe415086bdaaa1ce
UpendraChaturvedi <upendrachaturvedi099@gmail.com>
2025-09-04 11:59:24 +0530

    Create 2819-RemoveTrailingZerosFromAString

---
+++ 2819-RemoveTrailingZerosFromAString/2819-RemoveTrailingZerosFromAString.java
@@ -0,0 +1,12 @@
+// Last updated: 9/4/2025, 11:59:24 AM
+class Solution {
+    public String removeTrailingZeros(String num) {
+        int i = num.length() - 1;
+        while (i >= 0 && num.charAt(i) == '0') {
+            i--;
+        }
+        return num.substring(0, i + 1);
+    
+        
+    }
+}
