3dde4d741c8834783bd58325968372fd51b649dd
UpendraChaturvedi <upendrachaturvedi099@gmail.com>
2025-09-04 12:08:03 +0530

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.java
@@ -0,0 +1,13 @@
+// Last updated: 9/4/2025, 12:08:03 PM
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxind=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(i>maxind) return false;
+            maxind=Math.max(maxind,i+nums[i]);
+        }
+        return true;
+    
+    }
+}
