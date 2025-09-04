239d564dba0ba19dc1fa26fdd87ab16f42d7e122
UpendraChaturvedi <upendrachaturvedi099@gmail.com>
2025-09-04 11:59:26 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,16 @@
+// Last updated: 9/4/2025, 11:59:26 AM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+        
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
