245655dce6f2136676d2afc9b81551b3c3dbdd67
UpendraChaturvedi <upendrachaturvedi099@gmail.com>
2025-09-04 12:07:16 +0530

    Create 111-MinimumDepthOfBinaryTree

---
+++ 111-MinimumDepthOfBinaryTree/111-MinimumDepthOfBinaryTree.java
@@ -0,0 +1,26 @@
+// Last updated: 9/4/2025, 12:07:16 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root==null) return 0;
+        if(root.left==null) return minDepth(root.right)+1;
+        else if(root.right==null) return minDepth(root.left)+1;
+        else{
+            return Math.min(minDepth(root.left),minDepth(root.right))+1;
+        }
+    }
+}
